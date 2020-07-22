3faa151c02a3edccf38f6133fb070ea4ffc1f028
Jana Kurrek <janakurrek@icloud.com>
2020-07-22 18:43:36 -0400

    Initial Commit

---
+++ utils/load_data.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+MNLI_PATH = "multinli_1.0_train.jsonl"
+GLOVE_PATH = "embeddings/glove.6B.50d.txt"
+LABEL_TO_INT = {'contradiction':1, 'entailment':2, 'neutral':3}
+
+def load_mnli():
+    # read data to pandas dataframe 
+    mnli_data = pd.read_json(MNLI_PATH, lines=True)
+    # combine pairs and map labels to ids
+    mnli_data["sentence"] = mnli_data["sentence1"] + "<END_OF_PAIR>" + mnli_data["sentence2"]
+    mnli_data["gold_label"] = mnli_data["gold_label"].apply(lambda label: LABEL_TO_INT[label])
+    # split data into train, validation, and test set
+    train, validate, test = np.split(
+        mnli_data.sample(frac=1), [int(.6*len(mnli_data)),int(.8*len(mnli_data))]
+    )
+    # export to csv and return train, validation, and test set
+    train.to_csv("train.csv")
+    validate.to_csv("val.csv")
+    test.to_csv("test.csv")
+    return train, validate, test
+
+def load_glove(vocabulary):
+    """
+    Wikipedia 2014 + Gigaword 5 vectors
+    https://nlp.stanford.edu/projects/glove/
+    """
+    embeddings = {}
+  
+    with open(GLOVE_PATH, 'r', encoding="utf-8") as f:
+        for line in f:
+            values = line.split()
+            word, vector = values[0], np.asarray(values[1:], "float32")
+            if word in vocabulary:
+                embeddings[word] = vector
+    return embeddings
+
+if __name__ == "__main__":
+    load_mnli()
---
+++ utils/vocab.py
@@ -0,0 +1,55 @@
+import re 
+import spacy
+import numpy as np
+
+from collections import Counter
+
+nlp = spacy.load('en', parser=False, entity=False, tagger=False, textcat=False)
+
+contraction_mapping = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",  "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",  "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have" }
+
+mispell_dict = {'colour': 'color', 'centre': 'center', 'favourite': 'favorite', 'travelling': 'traveling', 'counselling': 'counseling', 'theatre': 'theater', 'cancelled': 'canceled', 'labour': 'labor', 'organisation': 'organization', 'wwii': 'world war 2', 'citicise': 'criticize', 'youtu ': 'youtube ', 'Qoura': 'Quora', 'sallary': 'salary', 'Whta': 'What', 'narcisist': 'narcissist', 'howdo': 'how do', 'whatare': 'what are', 'howcan': 'how can', 'howmuch': 'how much', 'howmany': 'how many', 'whydo': 'why do', 'doI': 'do I', 'theBest': 'the best', 'howdoes': 'how does', 'mastrubation': 'masturbation', 'mastrubate': 'masturbate', "mastrubating": 'masturbating', 'pennis': 'penis', 'Etherium': 'Ethereum', 'narcissit': 'narcissist', 'bigdata': 'big data', '2k17': '2017', '2k18': '2018', 'qouta': 'quota', 'exboyfriend': 'ex boyfriend', 'airhostess': 'air hostess', "whst": 'what', 'watsapp': 'whatsapp', 'demonitisation': 'demonetization', 'demonitization': 'demonetization', 'demonetisation': 'demonetization'}
+
+char_mapping = {"‘": "'", "₹": "e", "´": "'", "°": "", "€": "e", "™": "tm", "√": " sqrt ", "×": "x", "²": "2", "—": "-", "–": "-", "’": "'", "_": "-", "`": "'", '“': '"', '”': '"', '“': '"', "£": "e", '∞': 'infinity', 'θ': 'theta', '÷': '/', 'α': 'alpha', '•': '.', 'à': 'a', '−': '-', 'β': 'beta', '∅': '', '³': '3', 'π': 'pi', }
+
+def clean_string(s):
+    s = s.lower().strip()
+    s = re.sub(r"([.,!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    
+    for dic in [contraction_mapping, mispell_dict, char_mapping]:
+        for word in dic.keys():
+            s = s.replace(word, dic[word])
+    return s
+
+def tokenize(s):
+    return [str(s) for s in nlp(s)]
+
+def get_labels_ix_dict(labels):
+    return dict(zip(labels, range(len(labels))))
+
+def get_vocabulary(train_df):
+    tokens = []
+    counter = Counter()
+    for sentence_pair in zip(train_df["clean1"],train_df["clean2"]):
+        tokens1, tokens2 = tokenize(sentence_pair[0]), tokenize(sentence_pair[1])
+        counter.update(tokens1)
+        counter.update(tokens2)
+        tokens.append(tokens1 + tokens2)
+    vocabulary = list(set([word for word in counter]))
+    vocabulary = ["<PAD>", "<UNK>"] + vocabulary
+    return vocabulary, tokens
+
+def get_weights_matrix(embeddings, word_to_ix, dims, oov_init_zero=False):
+    
+    M = np.zeros(dims)
+    vocabulary_dim, word_embedding_dim = dims
+    
+    for word, i in word_to_ix.items():
+        try:
+            M[i] = embeddings[word]
+        except KeyError:
+            if oov_init_zero:
+                embeddings[word] = np.random.normal(scale=0.6, size=(1,word_embedding_dim))
+            continue
+    return M
